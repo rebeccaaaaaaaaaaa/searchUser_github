66f5fb9fb8b6ac22ca9b17843266c68559006acf
rebeccaaaaaaaaaaa <rebeccaignacio1996@gmail.com>
2022-04-14 20:23:48 -0300

    Finalizando o projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
 
 function App() {
+
+  const [userName, setUserName] = useState("");
+  const [user, setUser] = useState("");
+  const [login, setLogin] = useState("");
+  const [repos, setRepos] = useState(0);
+  const [followers, setFollowers] = useState(0);
+  const [following, setFollowing] = useState(0);
+  const [avatar, setAvatar] = useState("");
+  const [joined, setJoined] = useState("");
+  const [location, setLocation] = useState("");
+  const [bio, setBio] = useState("");
+  const [company, setCompany] = useState("");
+  const [blog, setBlog] = useState("");
+  const [twitter, setTwitter] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setUser(e.target.value);
+    console.log(user);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`https://api.github.com/users/${user}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setUserName(data.name);
+        setBio(data.bio);
+        setJoined(data.created_at);
+        setLogin(data.login);
+        setRepos(data.public_repos);
+        setFollowers(data.followers);
+        setFollowing(data.following);
+        setAvatar(data.avatar_url);
+        setLocation(data.location);
+        setCompany(data.company);
+        setBlog(data.blog);
+        setTwitter(data.twitter_username);
+        setAvatar(data.avatar_url);
+      
+        console.log(data);
+      });
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="contentWrapper">
+        <Header />
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Pesquisar"
+            className="input"
+            onChange={handleSearch}
+          />
+          <button className="buttonSearch" onClick={handleSubmit}>
+            buscar
+          </button>
+        </div>
+        {
+          // verificar se tem dados
+          login ? (
+            <div className="results">
+              <div className="resultImage">
+                <img src={avatar} alt="imagem" />
+              </div>
+              <div className="result-content">
+                <div className="resultInfoHeader">
+                  <div className="result-info-header--wrapper">
+                    <h2 className="result-info--name"> {userName ? userName : "Sem nome"} </h2>
+                    <small className="result-info-user"> @{login ? login : "no info"} </small>
+                  </div>
+                  <span className="resultInfoJoined">
+                    Joined
+                    {joined ? joined : "Sem informação"}
+                  </span>
+                </div>
+
+                <div className="result-info-bio">
+                  <p> {bio ? bio : "Sem biografia"} </p>
+                </div>
+
+                <div className="resultInfoReposInfo">
+                  <div className="resultInfoReposInfoRepos">
+                    <span> Repos </span>
+                    <span> {repos ? repos : "0"} </span>
+                  </div>
+                  <div className="resultInfoReposInfoFollowers">
+                    <span> Followers </span>
+                    <span> {followers ? followers : "0"} </span>
+                  </div>
+                  <div className="resultInfoReposInfoFollowing">
+                    <span> Following </span>
+                    <span> {following ? following : "0"} </span>
+                  </div>
+                </div>
+
+                <div className="resultInfoHeaderWrapper">
+                  <div className="adicionalInfoLocation">
+                    <span> {location ? location : "Sem informação"} </span>
+                  </div>
+                  <div className="adicionalInfoSite">
+                    <a href=" "> {blog ? blog : "Sem informação" } </a>
+                  </div>
+                  <div className="adicionalInfoSocial">
+                    <a href=" "> {twitter ? twitter : "Sem informação"} </a>
+                  </div>
+                  <div className="adicionalInfoOrganization">
+                    <span> {company ? company : "Sem informação"} </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <p> Informações não encontradas</p>
+          )
+        }
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Header.module.css"
+
+// import { Container } from './styles';
+
+function Header() {
+  return (
+    <div className={styles.header}>
+      <h1>defvfinder</h1>
+     {/*  <button> mudar tema </button> */}
+    </div>
+  );
+}
+
+export default Header;
